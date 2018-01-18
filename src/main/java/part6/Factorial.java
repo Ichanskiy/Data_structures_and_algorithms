b3ceaaa098b9370b339a40be45f7b5482f581f5c
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-01-18 14:20:51 +0200

    add calculate Factorial

---
+++ src/main/java/part6/Factorial.java
@@ -0,0 +1,16 @@
+package part6;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(calculateFactorial(n));
+    }
+
+    private static int calculateFactorial(int n){
+        if (n == 0) {
+            return 1;
+        }
+        return (n * calculateFactorial(n - 1));
+    }
+}
