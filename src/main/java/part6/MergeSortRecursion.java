3f697d4abf0f3bb64fde124447e02969def7fd35
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-01-30 14:29:13 +0200

    update merge sort recursion

--- src/main/java/part6/MergeSortRecursion.java
+++ src/main/java/part6/MergeSortRecursion.java
@@ -1,9 +1,9 @@
 package part6;
 
-class DArray
-{
+class DArray {
     private long[] theArray; // Ссылка на массив theArray
     private int nElems; // Количество элементов данных
+
     //-----------------------------------------------------------
     public DArray(int max) // Конструктор
     {
@@ -17,63 +17,65 @@ class DArray
         theArray[nElems] = value; // Вставка элемента
         nElems++; // Увеличение размера
     }
+
     //-----------------------------------------------------------
     public void display() // Вывод содержимого массива
     {
-        for(int j=0; j<nElems; j++) // Для каждого элемента
+        for (int j = 0; j < nElems; j++) // Для каждого элемента
             System.out.print(theArray[j] + " "); // Вывод
         System.out.println("");
     }
+
     //-----------------------------------------------------------
     public void mergeSort() // Вызывается из main()
     { // Рабочая область
         long[] workSpace = new long[nElems];
-        recMergeSort(workSpace, 0, nElems-1);
+        recMergeSort(workSpace, 0, nElems - 1);
     }
+
     //-----------------------------------------------------------
     private void recMergeSort(long[] workSpace, int lowerBound,
-                              int upperBound)
-    {
-        if(lowerBound == upperBound) // Если только один элемент,
+                              int upperBound) {
+        if (lowerBound == upperBound) // Если только один элемент,
             return; // сортировка не требуется.
         else
+
         { // Поиск середины
-            int mid = (lowerBound+upperBound) / 2;
+            int mid = (lowerBound + upperBound) / 2;
             // Сортировка нижней половины
             recMergeSort(workSpace, lowerBound, mid);
             // Сортировка верхней половины
-            recMergeSort(workSpace, mid+1, upperBound);
+            recMergeSort(workSpace, mid + 1, upperBound);
             // Слияние
-            merge(workSpace, lowerBound, mid+1, upperBound);
+            merge(workSpace, lowerBound, mid + 1, upperBound);
         }
     }
+
     //-----------------------------------------------------------
     private void merge(long[] workSpace, int lowPtr,
-                       int highPtr, int upperBound)
-    {
+                       int highPtr, int upperBound) {
         int j = 0; // Индекс в рабочей области
         int lowerBound = lowPtr;
-        int mid = highPtr-1;
-        int n = upperBound-lowerBound+1; // Количество элементов
-        while(lowPtr <= mid && highPtr <= upperBound)
-            if( theArray[lowPtr] < theArray[highPtr] )
+        int mid = highPtr - 1;
+        int n = upperBound - lowerBound + 1; // Количество элементов
+        while (lowPtr <= mid && highPtr <= upperBound)
+            if (theArray[lowPtr] < theArray[highPtr])
                 workSpace[j++] = theArray[lowPtr++];
             else
-        workSpace[j++] = theArray[highPtr++];
-        while(lowPtr <= mid)
+                workSpace[j++] = theArray[highPtr++];
+        while (lowPtr <= mid)
             workSpace[j++] = theArray[lowPtr++];
-        while(highPtr <= upperBound)
+        while (highPtr <= upperBound)
             workSpace[j++] = theArray[highPtr++];
-        for(j=0; j<n; j++)
-            theArray[lowerBound+j] = workSpace[j];
+        for (j = 0; j < n; j++)
+            theArray[lowerBound + j] = workSpace[j];
     }
 //-----------------------------------------------------------
 } // Конец класса DArray
+
 ////////////////////////////////////////////////////////////////
-class MergeSortApp
-{
-    public static void main(String[] args)
-    {
+class MergeSortApp {
+    public static void main(String[] args) {
         int maxSize = 100; // Размер массива
         DArray arr; // Ссылка на массив
         arr = new DArray(maxSize); // Создание массива
