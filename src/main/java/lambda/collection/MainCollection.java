46164c42ff92904c767be2fada46611dc9ac01e0
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-05-30 13:49:14 +0300

    add something method to lambda

--- src/main/java/lambda/collection/MainCollection.java
+++ src/main/java/lambda/collection/MainCollection.java
@@ -1,8 +1,6 @@
 package lambda.collection;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MainCollection {
@@ -13,6 +11,7 @@ public class MainCollection {
         people.add(new Person("lol", 120));
         people.add(new Person("aek", 110));
         people.add(new Person("def", 20));
+        people.add(new Person("def", 30));
 
         people.stream().sorted(Comparator.comparing(Person::getAge)).filter(person -> person.getAge() > 28).collect(Collectors.toList()).forEach(System.out::println);
         System.out.println("");
@@ -23,6 +22,26 @@ public class MainCollection {
 
         System.out.println("average");
         System.out.println(people.stream().filter(person -> person.getAge() > 100).mapToDouble(Person::getAge).average().getAsDouble());
-    }
 
+        System.out.println("maxValue");
+        int maxValue = people.stream().filter(Objects::nonNull).map(Person::getAge).max(Integer::compareTo).get();
+        System.out.println("maxValue = " + maxValue);
+
+        System.out.println("count");
+        long count = people.parallelStream().filter(person -> person.getName().equals("def")).count();
+        System.out.println("count = " + count);
+
+        System.out.println("limit");
+        System.out.println(people.stream().limit(2).collect(Collectors.toList()));
+
+        System.out.println("---------");
+        List<Person> people1 = people.stream().filter(person -> person.getAge() > 28).map(person -> {
+            person.setAge(10);
+            return person;
+        }).collect(Collectors.toList());
+        System.out.println("people age = 10 = " + people1);
+
+        List<Person> people2 = people.stream().filter(person -> person.getAge() > 9).peek(person -> person.setAge(10)).collect(Collectors.toList());
+        System.out.println("people age = 10 = " + people2);
+    }
 }
